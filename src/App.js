4c4af3a85e4b3212cc49ef38d066367e9033df9d
romankt21 <romankt21@gmail.com>
2022-09-17 16:26:56 +0300

    in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import User from "./components/user/User";
 
 function App() {
     return (
         <div >
             <h1>hello</h1>
-            <User name={'vasyl'} age={31}/>
-            <User name={'petro'} age={41}/>
+
 
 
         </div>
