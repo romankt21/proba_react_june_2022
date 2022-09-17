607ee7f9e25290197731c4be881c5c41d7ba41ab
romankt21 <romankt21@gmail.com>
2022-09-17 11:40:54 +0300

    in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
 import User from "./components/user/User";
-import SimpsonComponent from "./components/user/SimpsonComponent";
 
 function App() {
     return (
@@ -10,9 +9,7 @@ function App() {
             <User name={'vasyl'} age={31}/>
             <User name={'petro'} age={41}/>
 
-            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/f15f5662080793.5a8432e3d5b6b.jpg"/>
 
-            <SimpsonComponent/>
         </div>
     );
 
