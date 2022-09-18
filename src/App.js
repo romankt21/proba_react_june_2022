fd0cbfa56cc26899b2904817b7045e25be311f6f
romankt21 <romankt21@gmail.com>
2022-09-18 18:19:05 +0300

    in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 // import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {getUser, getUsers} from "./services/user.service";
+import UserComponent from "./components/UserComponent";
+import {logDOM} from "@testing-library/react";
 
 function App() {
+
+    let [users, setUsers] = useState([]);
+    let [choosenUser, setChoosenUser] = useState( null);
+
+    useEffect(() => {
+        getUsers().then(value => setUsers(value.data))
+
+    }, [])
+
+    const chooseUser = (id) => {
+        getUser(id).then(value => setChoosenUser(value.data));
+    }
+
+
+
     return (
         <div >
-            <h1>hello</h1>
 
-
+            {
+                choosenUser && (<div>{choosenUser.name}</div>)
+            }
+            <hr/>
+            {users.map(value => <
+                UserComponent
+                key = {value.id}
+                item={value}
+                chooseUser={chooseUser}
+            />)}
 
         </div>
     );
