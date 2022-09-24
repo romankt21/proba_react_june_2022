0ce6c24aa3a39081c001379bcbd144cc83b25333
romankt21 <romankt21@gmail.com>
2022-09-24 13:54:33 +0300

    in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 // import logo from './logo.svg';
 import './App.css';
 
+
+
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    link,
+    Outlet, Link
+} from "react-router-dom";
+import Home from "./components/home/Home";
+
+
 function App() {
     return (
         <div >
-            <h1>hello</h1>
+            {/*<h1>hello</h1>*/}
+            <ul>
+                <li><Link to={'/'}>Home page</Link></li>
+                <li><Link to={'/users'}>all users page</Link></li>
+                <li><Link to={'/posts'}>all posts page</Link></li>
+
+
+            </ul>
+
+            <hr/>
+            <Routes>
+
+                <Route path={'/'} element={<Home/>}/>
+
+
+            </Routes>
+
+            <hr/>
+
+
+
 
 
 
