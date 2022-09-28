9b7a020d0a911974c1c93f2733f501b1e7fbd7a9
romankt21 <romankt21@gmail.com>
2022-09-28 22:37:17 +0300

    in

---
+++ src/components/Test.js
@@ -0,0 +1,17 @@
+import {Component} from "react";
+
+class Test extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>Hello from class component</div>
+        )
+    }
+
+}
+
+
+export {Test}
