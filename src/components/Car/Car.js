842a05271d607e2932ce643d829285de04e0f0d8
romankt21 <romankt21@gmail.com>
2022-10-29 10:39:04 +0300

    in

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -1,11 +1,20 @@
+import css from './Car.module.css';
+
 const Car = ({car}) => {
     const {id, model, price, year} = car;
     return (
-        <div>
+        <div className={css.Car}>
+            <div>
+
             <div>id: {id}</div>
             <div>model: {model}</div>
             <div>price: {price}</div>
             <div>year: {year}</div>
+            </div>
+            <div className={css.tools}>
+                <button>Update</button>
+                <button>Delete</button>
+            </div>
         </div>
     )
 };
--- src/components/CarForm/CarForm.js
+++ src/components/CarForm/CarForm.js
@@ -1,9 +1,49 @@
-const CarForm = () => {
+import {useForm} from "react-hook-form";
+import {joiResolver} from '@hookform/resolvers/joi';
+
+import {carValidator} from "../../validators";
+import {carService} from "../../services";
+import {useEffect} from "react";
+
+const CarForm = ({setCars}) => {
+    const {register, handleSubmit, reset, formState: {errors, isValid}, setValue} = useForm({
+        resolver:joiResolver(carValidator),
+        mode:'all'
+    });
+
+    useEffect(()=> {
+        setValue('model', '')
+    },[])
+
+    const submit = async (car) => {
+       const {data} = await carService.create(car);
+        setCars(cars=>[...cars, data])
+        reset ()
+
+    };
+
     return (
-        <div>
-            CarForm
-        </div>
-    )
+        // <form onSubmit={handleSubmit(submit)} onChange={()=> console.log(errors)}>
+        //     <input type="text" placeholder={'model'} {...register('model', {required: true, minLength: {value: 2,message: 'min 2ch'}})}/>
+        //     {errors.model&&<span>{errors.model.message}</span>}
+        //     <input type="text" placeholder={'price'} {...register('price', {valueAsNumber: true})}/>
+        //     <input type="text" placeholder={'year'} {...register('year', {valueAsNumber: true})}/>
+        //     <button disabled={!isValid}>Save</button>
+        // </form>
+
+    <form onSubmit={handleSubmit(submit)}>
+            <input type="text" placeholder={'model'} {...register('model')}/>
+            {errors.model&&<span>{errors.model.message}</span>}
+            <input type="text" placeholder={'price'} {...register('price', {valueAsNumber: true})}/>
+            {errors.price&&<span>{errors.price.message}</span>}
+            <input type="text" placeholder={'year'} {...register('year', {valueAsNumber: true})}/>
+            {errors.year&&<span>{errors.year.message}</span>}
+            <button disabled={!isValid}>Save</button>
+        </form>
+
+
+
+    );
 };
 
 export {CarForm};
